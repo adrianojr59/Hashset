4e558b761a42cc3294537adce4bc2c0b1ee9d3de
adria <adria@ADRIANO>
2021-12-10 16:52:55 -0300

    Adicionar arquivos de projeto.

---
+++ Hashset.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Hashset
+{
+
+    public class Produto
+    {
+        public int Id { get; set; } 
+        public  string Nome { get; set; }
+
+        public Produto (int id , string nome)
+        {
+
+            Id=id;
+            Nome=nome;
+
+        }
+       
+
+        public Produto() { }
+       
+       
+
+        public HashSet<Produto> hashProduto = new HashSet<Produto>(new ProdutoComparar());
+
+
+        private class ProdutoComparar : IEqualityComparer<Produto> 
+        {
+            public bool Equals(Produto? x, Produto? y)
+            {
+                return x.Nome.Equals(y.Nome,StringComparison.CurrentCultureIgnoreCase);   
+            }
+
+
+            public int GetHashCode(Produto obj)
+            {
+                return obj.Nome.ToUpper().GetHashCode();
+            }
+        }
+
+
+
+
+
+    
+          
+
+
+
+
+
+    public override string ToString()
+        {
+            return "ID "+Id + "\nNOME: "+Nome ;
+        }
+
+        
+    }
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using Hashset;
+
+char option;
+
+
+
+Produto prod = new Produto();
+
+do { 
+
+Console.WriteLine("ID: ");
+int id =int.Parse(Console.ReadLine());
+
+Console.WriteLine("NOME: ");
+string nome = Console.ReadLine();
+
+
+     prod.Id = id;
+     prod.Nome = nome ;
+    Console.WriteLine(prod.hashProduto.Contains(prod));
+
+    if (prod.hashProduto.Contains(prod))
+    {
+        Console.WriteLine("produto Já Existe");
+
+    }
+    else 
+    
+
+        prod.hashProduto.Add(new Produto(id ,nome));
+
+    
+    Console.WriteLine("Cadastrar (S/N) ");
+    option = char.Parse(Console.ReadLine());
+}
+
+while (option == 's' || option == 'S');
+
+
+
+
+   
+
+foreach (var item in  prod.hashProduto )
+{
+   
+    Console.WriteLine(item);
+    Console.WriteLine("HashCode " + item.GetHashCode());
+
+}
+
+
+
